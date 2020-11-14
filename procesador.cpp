ee0e4395a9eb896f848782792286fee4c7c044af
theccromer <carlos.urenaalfaro@gmail.com>
2020-11-13 22:02:14 -0600

    addi sub div lw y bne. Metodo asociar

--- procesador.cpp
+++ procesador.cpp
@@ -11,14 +11,14 @@ void Controlador::add(){
 
 }
 
-void Controlador::addi()
+void Controlador::addi(int x1, int x2, int n)
 {
-
+    hilos[0].registros[x1] = hilos[0].registros[x2] + n; //?? Ese 0 se tendria que sustituir por un entero que apunte al hilo actual
 }
 
-void Controlador::sub()
+void Controlador::sub(int x1, int x2, int x3)
 {
-
+    hilos[0].registros[x1] = hilos[0].registros[x2] - hilos[0].registros[x3];
 }
 
 void Controlador::mul( int x1, int x2, int x3 )
@@ -26,14 +26,15 @@ void Controlador::mul( int x1, int x2, int x3 )
     hilos[0].registros[x1] = hilos[0].registros[x2] * hilos[0].registros[x3];
 }
 
-void Controlador::div()
+void Controlador::div(int x1, int x2, int x3)
 {
-
+    hilos[0].registros[x1] = hilos[0].registros[x2] / hilos[0].registros[x3];
 }
 
-void Controlador::lw()
+void Controlador::lw(int x1, int x2, int n)
 {
-
+    int direccion = hilos[0].registros[x2] + n;
+    hilos[0].registros[x1] = memoria.datos[direccion]; // x1 <- M[x2 + n]
 }
 
 void Controlador::sw()
@@ -46,9 +47,10 @@ void Controlador::beq()
 
 }
 
-void Controlador::bne()
+void Controlador::bne(int x1, int x2, int n)
 {
-
+    if(hilos[0].registros[x1] != hilos[0].registros[x2])
+        hilos[0].PC += n * 4; // PC <- n * 4
 }
 
 void Controlador::lr()
@@ -83,9 +85,55 @@ void Controlador::aumentar_reloj()
 
 }
 
-void Controlador::asociar(int codigo)
+void Controlador::asociar(int codigo, int x, int y, int z) //Si no se ocupa un parametro para un metodo se pasa un cero y no se hace nada con el 
 {
-
+    switch (codigo)
+    {
+    case 19:
+        addi(x,y,z);
+        break;
+    case 71:
+        add(x,y,z);
+        break;
+    case 83:
+        sub(x,y,z);
+        break;
+    case 72:
+        mul(x,y,z);
+        break;
+    case 56:
+        div(x,y,z);
+        break;
+    case 5:
+        lw(x,y,z);
+        break;
+    case 37:
+        sw(x,y,z);
+        break;
+    case 99:
+        beq(x,y,z);
+        break;
+    case 100:
+        bne(x,y,z);
+        break;
+    case 51:
+        lr(x,y,z);
+        break;
+    case 52:
+        sc(x,y,z);
+        break;
+    case 111:
+        jal(x,z);
+        break;
+    case 103:
+        jalr(x,y,z);
+        break;
+    case 999:
+        FIN();
+        break;
+    default:
+        break;
+    }
 }
 
 void Controlador::buffer_victima()
--- procesador.h
+++ procesador.h
@@ -34,18 +34,18 @@ class Controlador
     Controlador();
     ~Controlador();
     void aumentar_reloj();
-    void asociar(int codigo); // para asociar los codigos de operación
+    void asociar(int codigo, int x, int y, int z); // para asociar los codigos de operación
     void buffer_victima();
     void cambio_contexto();
     void add(); //fabian
-    void addi(); //carlos
-    void sub(); //carlos
+    void addi(int x1, int x2, int n); //carlos
+    void sub(int x1, int x2, int x3); //carlos
     void mul( int x1, int x2, int x3 ); //roy
-    void div(); //carlos 
-    void lw(); //carlos
+    void div(int x1, int x2, int x3); //carlos 
+    void lw(int x1, int x2, int n); //carlos
     void sw(); //fabian
     void beq(); //fabian
-    void bne(); //carlos
+    void bne(int x1, int x2, int n); //carlos
     void lr(); //roy
     void sc(); //fabian
     void jal( int x1, int n ); //roy
@@ -58,5 +58,7 @@ class Controlador
     int quantum;
     Hilo * hilos;
     int buffer[16];
+    Memoria memoria;
+
 };
 #endif
