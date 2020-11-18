677a5f839bb1261d8c925e60ba241617d6e14ebd
Fabian Jimenez <fabian.jimenezartola@ucr.ac.cr>
2020-11-18 00:46:42 -0600

    sc listo, cambios en sw para que tenga el mismo orden de parametros

--- procesador.h
+++ procesador.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <fstream>
 #include <iostream>
+#include <string>
+
 //#include <pthread.h>
 
 struct Hilo
@@ -68,11 +70,11 @@ class Controlador
     void mul( int x1, int x2, int x3 ); //roy
     void div(int x1, int x2, int x3); //carlos 
     void lw(int x1, int x2, int n); //carlos
-    void sw(int x1, int x2, int n); //fabian
+    void sw(int x2, int x1, int n); //fabian
     void beq(int x1, int x2, int n); //fabian
     void bne(int x1, int x2, int n); //carlos
     void lr( int x1, int x2 ); //roy
-    void sc(); //fabian
+    void sc(int x2, int x1, int n); //fabian
     void jal( int x1, int n ); //roy
     void jalr( int x1, int x2, int n );//roy
     void FIN(); // En este metodo se deberia restar la longitud del vector de hilos
